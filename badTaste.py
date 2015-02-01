aca836bf56dc05787c9d949ee4dc6f15aa32f5e3
Chad Nelson <chadbnelson@gmail.com>
2015-01-31 22:23:12 -0500

    Switch dish selection to redis

--- badTaste.py
+++ badTaste.py
@@ -1,59 +1,41 @@
-__author__ = 'cbn'
-
 import re
 from random import randrange
-import csv
 from nltk import tokenize
 from nltk.corpus import cmudict
 from redis import StrictRedis
 
+
 class BadTaste:
 
-    def __init__(self, csvfile=None):
-        if csvfile:
-            self.load_dishes(csvfile)
-            self.dish_count = len(self.dishes)
-        if self.dishes:
-            self.dish = self.get_dish()
-
-    def load_dishes(self,csvfile):
-        with open(csvfile) as dishes:
-            self.dishes =  [dish['name'] for dish in csv.DictReader(dishes)]
+    def __init__(self):
+        self.dish = self.get_dish()
 
     def get_dish(self):
-        if self.dishes:
-            self.dish = self.dishes[randrange(self.dish_count)]
-            self.dishwords = self.get_dish_words()
-            return self.dish
-        else:
-            raise ValueError("I don't know about no dishes")
+        red = StrictRedis(db=0)
+        self.dish = red.randomkey().decode('utf-8')
+        self.dishwords = self.get_dish_words()
+        return self.dish
 
     def get_dish_words(self):
         regex = re.compile('[^A-Za-z ]')
         return regex.sub('', self.dish).strip().split(" ")
 
-    def get_dish_word(self):
-        return self.dishwords[randrange(len(self.dishwords))]
-
-    def get_random_dish(self):
-        return self.get_dish_word(self.dishwords)
-
     def get_bad_dish(self):
         def find_rhyme_in_words(dish, dishwords, redis, replaceCount=0):
             for word in sorted(dishwords):
-                if word.lower() not in ["a", "an", "and", "of", "the", "with",]:
+                if word.lower() not in ["a", "an", "and", "of", "on", "the", "with",]:
                     pronunciations = cmudict.dict().get(word.lower(), [])
                     for pron in pronunciations:
                         badWord = find_best_rhyme(pron, redis)
                         if badWord:
-                            dish = dish.replace(word, badWord.decode('utf-8')).title()
+                            dish = dish.replace(word, badWord.decode('utf-8'))
                             replaceCount += 1
                             break
-                    if replaceCount / len(dishwords) < (1/3):
+                    if (replaceCount / len(dishwords)) < (1/3):
                         continue
                     else:
                         break
-            return dish
+            return dish.title()
 
         def find_best_rhyme( pronunciation, redis):
             for i in range(len(pronunciation)-1,1,-1):
@@ -67,10 +49,11 @@ class BadTaste:
             redis_url = "localhost"
             self.redis = StrictRedis(host=redis_url, db=1)
         newDish = find_rhyme_in_words(self.dish, self.dishwords,self.redis)
-        self.get_dish()
-        if newDish != self.dish:
+        if newDish.lower() != self.dish.lower():
+            self.get_dish()
             return newDish
         else:
+            self.get_dish()
             self.get_bad_dish()
 
 
@@ -104,3 +87,19 @@ class BadWords:
         # (u'D', u'AH0', u'L'),
         # (u'IH1', u'D', u'AH0', u'L'),
         # (u'W', u'IH1', u'D', u'AH0', u'L')
+
+def print_bad():
+    try:
+        bt = BadTaste()
+        dish1 = bt.get_bad_dish()
+        dish2 = bt.get_bad_dish()
+        phrase = "Starter: " + dish1 + ", Main: " + dish2
+        while len(phrase) > 140:
+           phrase = " ".join(phrase.split()[:-1])
+        print(len(phrase))
+        print(phrase)
+    except:
+        print_bad()
+
+if __name__ == "__main__":
+    print_bad()
